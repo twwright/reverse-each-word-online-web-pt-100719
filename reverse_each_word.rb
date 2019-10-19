fdff95245f8b203961b1b6c7b358f87a91bf69a2
Thomas Wright <twwright@gmail.com>
2019-10-19 21:52:42 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(string)
   array = string.split
-  reversed = array.collect do |str|
-    str.reverse
-  end
+  reversed = array.collect {|str| str.reverse}
   reversed.join(" ")
 end
